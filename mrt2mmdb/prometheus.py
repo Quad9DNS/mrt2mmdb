59369fc308d4f652ec90fc7e366c863ad060fb45
John Todd <jtodd@loligo.com>
2025-11-23 21:23:50 -0800

    Fixed prometheus lines for node consumer
    Removed spurious comments and added type lines

--- mrt2mmdb/prometheus.py
+++ mrt2mmdb/prometheus.py
@@ -9,43 +9,50 @@ def output_prometheus(
 ):
     """Return the prometheus format output using a f-string templating"""
     return f"""#
-# durations are seconds
+# HELP mrt2mmdb_description_asn_prefixes number of prefixes
+# TYPE mrt2mmdb_description_asn_prefixes gauge
 mrt2mmdb_description_asn_prefixes {asn_stats[0]}
+# HELP mrt2mmdb_description_asn_prefixes_duration duration of prefix load
+# TYPE mrt2mmdb_description_asn_prefixes_duration gauge
 mrt2mmdb_description_asn_prefixes_duration {asn_stats[1]:.0f}
+# HELP mrt2mmdb_description_asn_prefixes_per_second number of prefixes processed per second
+# TYPE mrt2mmdb_description_asn_prefixes_per_second gauge
 mrt2mmdb_description_asn_prefixes_per_second {asn_stats[0]/asn_stats[1]:.2f}
-#
+# HELP mrt2mmdb_dictionary_load_prefixes number of dictionary prefixes loaded
+# TYPE mrt2mmdb_dictionary_load_prefixes gauge
 mrt2mmdb_dictionary_load_prefixes {prefix_stats[0]}
+# HELP mrt2mmdb_dictionary_load_prefixes_duration total duration of dictionary prefix loading
+# TYPE mrt2mmdb_dictionary_load_prefixes_duration gauge
 mrt2mmdb_dictionary_load_prefixes_duration {prefix_stats[1]:.0f}
+# HELP mrt2mmdb_dictionary_load_prefixes_per_second dictionary prefixes processed per second
+# TYPE mrt2mmdb_dictionary_load_prefixes_per_second gauge
 mrt2mmdb_dictionary_load_prefixes_per_second {prefix_stats[0]/prefix_stats[1]:.2f}
-#
+# HELP mrt2mmdb_conversions number of conversions
+# TYPE mrt2mmdb_conversions gauge
 mrt2mmdb_conversions {convert_stats[0]}
+# HELP mrt2mmdb_conversions_duration duration of conversions
+# TYPE mrt2mmdb_conversions_duration gauge
 mrt2mmdb_conversions_duration {convert_stats[1]:.0f}
+# HELP mrt2mmdb_conversions_per_second conversions per second
+# TYPE mrt2mmdb_conversions_per_second gauge
 mrt2mmdb_conversions_per_second {convert_stats[0]/convert_stats[1]:.2f}
-#
-# How many prefixes were not found in the Maxmind template file that we’re using as a source for names?
-#
+# HELP mrt2mmdb_prefixes_no_description prefixes with no description text found
+# TYPE mrt2mmdb_prefixes_no_description gaugte
 mrt2mmdb_prefixes_no_description {len(missing_stats)}
+# HELP mrt2mmdb_asn_no_description asns with no description text found
+# TYPE mrt2mmdb_asn_no_description gauge
 mrt2mmdb_asn_no_description {len(set(missing_stats))}
-#
-# When did this instance of the process start? Unix epoch seconds.
-#
+# HELP mrt2mmdb_lastrun_timestamp epoch timestamp of process start
+# TYPE mrt2mmdb_lastrun_timestamp gauge
 mrt2mmdb_lastrun_timestamp {files_stats[0]:.0f}
-#
-# This is the creationtime of the MRT file that is being parsed
-# Unix epoch seconds.  This is what we can use to see if somehow
-# our MRT file collection pipeline is “stuck” and not being updated.
-#
+# HELP mrt2mmdb_mrt_file_creation_timestamp epoch timestamp of mrt file creation time
+# TYPE mrt2mmdb_mrt_file_creation_timestamp gauge
 mrt2mmdb_mrt_file_creation_timestamp {files_stats[1]:.0f}
-#
-# This is the creationtime of the template MMDB file that is being parsed
-# Unix epoch seconds.  This is what we can use to see if somehow
-# our template MMDB file collection pipeline is “stuck” and not being updated.
-#
+# HELP mrt2mmdb_template_mmdb_file_creation_timestamp epoch timestamp of mmdb template file if present
+# TYPE mrt2mmdb_template_mmdb_file_creation_timestamp gauge
 mrt2mmdb_template_mmdb_file_creation_timestamp {files_stats[2]:.0f}
-#
-# Keep a version number so we can track behaviors of different variations
-# MUST BE NUMERIC ONLY, with a single decimal point.
-#
+# HELP mrt2mmdb_version version number of mrt2mmdb code
+# TYPE mrt2mmdb_version gauge
 mrt2mmdb_version 1.0
 """
 
